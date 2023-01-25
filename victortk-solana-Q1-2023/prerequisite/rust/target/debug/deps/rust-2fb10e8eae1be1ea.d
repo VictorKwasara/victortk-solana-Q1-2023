0cfa756879c8915f1591478308e85749b3e25361
professor_cypher <professorcodegeek@DESKTOP-87DPNVK.localdomain>
2023-01-25 21:39:45 +0200

    printing

---
+++ victortk-solana-Q1-2023/prerequisite/rust/target/debug/deps/rust-2fb10e8eae1be1ea.d
@@ -0,0 +1,6 @@
+/home/professorcodegeek/rust/wba/victortk-solana-Q1-2023/prerequisite/rust/target/debug/deps/rust-2fb10e8eae1be1ea: src/main.rs src/print.rs
+
+/home/professorcodegeek/rust/wba/victortk-solana-Q1-2023/prerequisite/rust/target/debug/deps/rust-2fb10e8eae1be1ea.d: src/main.rs src/print.rs
+
+src/main.rs:
+src/print.rs:
---
+++ victortk-solana-Q1-2023/prerequisite/rust/target/debug/deps/rust-9bb45ae5fa4e80f1.d
@@ -0,0 +1,6 @@
+/home/professorcodegeek/rust/wba/victortk-solana-Q1-2023/prerequisite/rust/target/debug/deps/rust-9bb45ae5fa4e80f1.rmeta: src/main.rs src/print.rs
+
+/home/professorcodegeek/rust/wba/victortk-solana-Q1-2023/prerequisite/rust/target/debug/deps/rust-9bb45ae5fa4e80f1.d: src/main.rs src/print.rs
+
+src/main.rs:
+src/print.rs:
